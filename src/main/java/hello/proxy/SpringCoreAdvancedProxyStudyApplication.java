6738374be6da89076b5968a951083c23aa5dd634
장정우 <jungwoo@lotte.net>
2024-11-20 14:34:58 +0900

    first commit

---
+++ src/main/java/hello/proxy/SpringCoreAdvancedProxyStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreAdvancedProxyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreAdvancedProxyStudyApplication.class, args);
+    }
+
+}
