e3f2d0f6d95222a96a4fdb08ab5de8e121a898c2
장정우 <jungwoo@lotte.net>
2024-11-22 18:40:27 +0900

    20241122 스프링 핵심 원리(고급편) - 프록시 패턴 v2

--- src/main/java/hello/proxy/SpringCoreAdvancedProxyStudyApplication.java
+++ src/main/java/hello/proxy/SpringCoreAdvancedProxyStudyApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 //@Import(AppV1Config.class)
-@Import(AppV2Config.class)
-//@Import({AppV1Config.class, AppV2Config.class})
+@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
