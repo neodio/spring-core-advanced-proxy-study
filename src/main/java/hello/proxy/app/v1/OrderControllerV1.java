34dfa7a1043a17a0157e6e84a7484f57e160c59b
장정우 <jungwoo@lotte.net>
2024-11-26 17:45:15 +0900

    20241126 스프링 핵심 원리(고급편) - 프록시 패턴과 데코레이션 패턴 정리

--- src/main/java/hello/proxy/app/v1/OrderControllerV1.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1.java
@@ -4,9 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping//스프링은 @Controller 또는 @RequestMapping 이 있어야 스프링 컨트롤러로 인식
 @ResponseBody
+@RestController //boot 3.X 부터는 @Controller나 @RestController가 있어야 동작
 public interface OrderControllerV1 {
 
     @GetMapping("/v1/request")
--- src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequestMapping
 @ResponseBody
+@RestController //boot 3.X 부터는 @Controller나 @RestController가 있어야 동작
 public class OrderControllerV2 {
 
     private final OrderServiceV2 orderService;
