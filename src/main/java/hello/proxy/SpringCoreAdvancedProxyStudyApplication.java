404e84aeef12ce88a50d86b12872a73eb7637bcc
장정우 <jungwoo@lotte.net>
2024-12-13 16:24:50 +0900

    20241213 스프링 핵심 원리(고급편) - 프록시 팩토리

--- src/main/java/hello/proxy/SpringCoreAdvancedProxyStudyApplication.java
+++ src/main/java/hello/proxy/SpringCoreAdvancedProxyStudyApplication.java
@@ -6,6 +6,8 @@ import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
 import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,9 +22,10 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import({DynamicProxyFilterConfig.class, AppV2Config.class})
+//@Import({DynamicProxyFilterConfig.class, AppV2Config.class})
 //@Import(ProxyFactoryConfigV1.class)
-//@Import(ProxyFactoryConfigV2.class)
+//@Import({ProxyFactoryConfigV1.class, AppV2Config.class})
+@Import(ProxyFactoryConfigV2.class)
 //@Import(BeanPostProcessorConfig.class)
 //@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
